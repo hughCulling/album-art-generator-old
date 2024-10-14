abeffee64493e45949c7793c5ee06f58370f375e
Hugh Culling <hughculling@icloud.com>
2024-10-14 22:57:18 +0100

    fixed-ts-error

--- src/app/api/gemini/route.ts
+++ src/app/api/gemini/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 // Make sure to include these imports:
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.API_KEY!);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export async function POST(req: Request) {
